4700bd6da0bfaa080ec94bcbf76ab2d148b459e9
Ishan Jain <contact@ishanjain.me>
2023-07-23 00:48:08 +0530

    Added updated code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use axum::{
-    extract::ConnectInfo,
-    http::{HeaderMap, StatusCode},
-    routing::get,
-    Router,
-};
+use axum::{extract::ConnectInfo, http::HeaderMap, routing::get, Router};
 use std::{
     net::{IpAddr, SocketAddr},
     str::FromStr,
@@ -11,13 +6,8 @@ use std::{
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
+    let app = Router::new().route("/", get(root));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 4004));
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
@@ -25,19 +15,20 @@ async fn main() {
         .unwrap();
 }
 
-async fn root<'a>(
-    ConnectInfo(addr): ConnectInfo<SocketAddr>,
-    headers: HeaderMap,
-) -> (StatusCode, String) {
-    (
-        StatusCode::OK,
-        headers.get("X-Forwarded-For").map_or_else(
-            || addr.ip().to_string(),
-            |v| {
-                v.to_str()
-                    .map(|v| IpAddr::from_str(v).map_or_else(|_| "".to_string(), |x| x.to_string()))
-                    .unwrap()
-            },
-        ),
+async fn root<'a>(ConnectInfo(addr): ConnectInfo<SocketAddr>, headers: HeaderMap) -> String {
+    headers.get("X-Forwarded-For").map_or_else(
+        || addr.ip().to_string(),
+        |v| {
+            v.to_str()
+                .map(|v| {
+                    v.split_once(',').map_or_else(
+                        || IpAddr::from_str(v).map_or_else(|_| "".to_string(), |x| x.to_string()),
+                        |(ip, _)| {
+                            IpAddr::from_str(ip).map_or_else(|_| "".to_string(), |x| x.to_string())
+                        },
+                    )
+                })
+                .unwrap()
+        },
     )
 }
